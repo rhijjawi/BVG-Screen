4449ecab6a0574155b173026f8fd95751778766b
fenykep <abel.fenykep@gmail.com>
2024-06-06 17:57:25 +0200

    return sorted array

--- test.py
+++ test.py
@@ -4,17 +4,17 @@ import math
 import time
 from datetime import datetime, timezone
 import pytz
+from rich.pretty import pprint
 
 
 async def fetch_data(session, url):
     async with session.get(url) as response:
         return await response.json()
 
-async def main():
-    berlin_tz = pytz.timezone("Europe/Berlin")
-    stations = ["900193002","900120014","900120001","900120025"]
-    urls = [f"https://v6.bvg.transport.rest/stops/{station}/departures?pretty=false&suburban=true&subway=true&tram=true&bus=true&ferry=false&express=false&regional=true" for station in stations]
+async def parse_destinations(stations):
     filtered_departures = []
+    berlin_tz = pytz.timezone("Europe/Berlin")
+    urls = [f"https://v6.bvg.transport.rest/stops/{station}/departures?pretty=false&suburban=true&subway=true&tram=true&bus=true&ferry=false&express=false&regional=true" for station in stations]
     async with aiohttp.ClientSession() as session:
         tasks = [fetch_data(session, url) for url in urls]
         responses = await asyncio.gather(*tasks)
@@ -26,6 +26,12 @@ async def main():
                         "direction": departure["direction"],
                         "minutes": math.floor((datetime.strptime(departure["when"], "%Y-%m-%dT%H:%M:%S%z")-datetime.now(berlin_tz)).seconds/60)
                     })
-        print(filtered_departures)
+    return filtered_departures
+
+async def main():
+    stations = ["900193002","900120014","900120001","900120025"]
+#    stations = ["900120025"]
+    filtered_departures = await parse_destinations(stations)
+    pprint(sorted(filtered_departures, key=lambda x: x['minutes']))
 
 asyncio.run(main())
